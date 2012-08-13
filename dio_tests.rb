e0a75dea396418d83a2768dca0c137f6436ec30b
sue445 <go.sueyoshi@drecom.co.jp>
2012-08-13 10:29:44 +0900

    refactor: exrtact method

---
+++ dio_tests.rb
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+#!/usr/bin/ruby
+
+def print_test_count(testcase_regexp)
+  user = `git config --get user.name`.strip
+  since_commit = ARGV[0] || "master"
+  git_log = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space #{since_commit}..HEAD"
+
+  p git_log
+
+  plus_grep = "#{git_log} | grep -E '^\\+#{testcase_regexp}' -c"
+  minus_grep = "#{git_log} | grep -E '^-#{testcase_regexp}' -c"
+
+  #p plus_grep
+  #p minus_grep
+
+  plus_count = `#{plus_grep}`.strip.to_i
+  minus_count = `#{minus_grep}`.strip.to_i
+
+  p "plus=#{plus_count}, minus=#{minus_count}, increment=#{plus_count - minus_count}"
+end
+
+
--- rspec.rb
+++ rspec.rb
@@ -5,21 +5,10 @@
 Usage: ruby dio_tests/rspec.rb <commit>
 =end
 
+$:.unshift File.dirname(__FILE__)
+require 'dio_tests'
+
 testcase_regexp = "\s*its?"
-user = `git config --get user.name`.strip
-since_commit = ARGV[0] || "master"
-git_log = "git log --author=#{user} --remove-empty --oneline --unified=0 --ignore-all-space #{since_commit}..HEAD"
 
-p git_log
-
-plus_grep = "#{git_log} | grep -E '^\\+#{testcase_regexp}' -c"
-minus_grep = "#{git_log} | grep -E '^-#{testcase_regexp}' -c"
-
-#p plus_grep
-#p minus_grep
-
-plus_count = `#{plus_grep}`.strip.to_i
-minus_count = `#{minus_grep}`.strip.to_i
-
-p "plus=#{plus_count}, minus=#{minus_count}, increment=#{plus_count - minus_count}"
+print_test_count testcase_regexp
 
